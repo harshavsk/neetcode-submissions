67a4b7c70b43924e8737fc78d7318fa0d57748a4
harshavsk <114129620+harshavsk@users.noreply.github.com>
2026-06-17 18:40:39 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        sizes, res = [],[]
+        for s in strs:
+            sizes.append(len(s))
+        for size in sizes:
+            res.append(str(size))
+            res.append(',')
+        res.append('#')
+        res.extend(strs)
+        return ''.join(res)
+    def decode(self, s: str) -> List[str]:
+        if not s:
+            return []
+        i=0
+        sizes = []
+        res = []
+        while i<len(s) and s[i]!='#':
+            temp = ''
+            while s[i]!=',':
+                temp = temp+s[i]
+                i+=1
+            sizes.append(int(temp))
+            i+=1
+        i+=1
+        for size in sizes:
+            res.append(s[i:i+size])
+            i+=size
+        return res
